f53bf98508bf7fc8075df15ad4ca2eecfeb20f80
mikygaleano <creatormikeonline@gmail.com>
2023-05-27 18:43:36 -0300

    cardHome

--- src/components/CardHome/CardHome.jsx
+++ src/components/CardHome/CardHome.jsx
@@ -1,16 +1,22 @@
+import style from '../CardHome/CardHome.module.css'
 
-
-export default function CardHome ({apiData}) {
+export default function CardHome ({data, icon}) {
 
     return (
         <>
-           {
-            apiData.length < 0? (
-                <h1>Cargando...</h1>
-            ): (
-                <h1>{apiData.name}</h1>
-            )
-           }
+            <h1>{data.city.name}</h1>
+            <section className={style.sectionCardHome}>
+                <figure>
+                    <img src={icon} alt={data.list[0].weather[0].description} />
+                </figure>
+                <div>
+                    <span>{data.list[0].weather[0].description}</span>
+                    <span>Temp {data.list[0].main.temp}</span>
+                    <span>Max {data.list[0].main.temp_max}</span>
+                    <span>Min {data.list[0].main.temp_min}</span>
+                    <span>Hum {data.list[0].main.humidity}</span>
+                </div>
+            </section>
         </>
     )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react"
 
 export default function Home () {
 
+
     const API_key = 'a778027f763862b1d99bef3a0a0c626d';
 
     const [apiData, setApiData] = useState([]);
@@ -24,13 +25,13 @@ export default function Home () {
     useEffect(()=>{
         const fetchData = async () => {
             if (lat !== undefined && lon !== undefined) {
-              let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`;
+              let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&lang=es&appid=${API_key}`;
       
               try {
                 const response = await fetch(url);
                 const data = await response.json();
-                console.log(data);
                 setApiData(data);
+                console.log(data)
               } catch (error) {
                 console.log(error);
               }
@@ -41,11 +42,13 @@ export default function Home () {
     y solo lo haga cuando se actualiza la ubicacion del geolocation */
     },[lat, lon]);
 
+    
+   let icon = `https://openweathermap.org/img/wn/${apiData.list[0].weather[0].icon}@2x.png`
 
     return (
         
         <>
-            <CardHome apiData={apiData}/>
+            <CardHome data={apiData} icon={icon}/>
         </>
         
     )
